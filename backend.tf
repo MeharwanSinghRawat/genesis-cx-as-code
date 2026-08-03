732c8048ea4a3a9f1a95b039e6dad117615219d0
Vikraman Radhakrishnan <vikraman.career@gmail.com>
2026-08-03 23:07:33 +0530

    Configure secure S3 backend for GitHub Actions

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "genesys-cloud-terraform-state-test"
-    key          = "genesys-cloud/test/users/terraform.tfstate"
-    region       = "ap-south-1"
+    bucket       = "genesys-cx-terraform-state-687259232475"
+    key          = "genesys-cloud/terraform.tfstate"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
