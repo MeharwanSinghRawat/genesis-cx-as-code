25bacb3e4d7bce6ab35455012b96aff138381876
Vikraman Radhakrishnan <vikraman.career@gmail.com>
2026-08-10 13:34:56 +0530

    Pushing the changes to the remote repo

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
-# CODEOWNERS validation for the CX as Code reviewers team
-
 provider "genesyscloud" {
   oauthclient_id     = var.genesyscloud_oauthclient_id
   oauthclient_secret = var.genesyscloud_oauthclient_secret
